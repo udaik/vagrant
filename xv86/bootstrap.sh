22331cd4c41ade1706df155a68d25f514a3806d9
udaikiran <s.udaikiran@gmail.com>
2022-11-08 11:55:21 +0530

    updated

---
+++ xv86/bootstrap.sh
@@ -0,0 +1,6 @@
+# Update Packages
+apt-get update
+# Upgrade Packages
+apt-get upgrade -y
+
+sudo apt-get install git build-essential gdb-multiarch qemu-system-misc gcc-riscv64-linux-gnu binutils-riscv64-linux-gnu -y
