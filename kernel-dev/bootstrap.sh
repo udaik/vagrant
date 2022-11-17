f7a42dcb2b32b7841671e690f268c03babb99494
udaikiran <s.udaikiran@gmail.com>
2022-11-17 21:31:49 +0530

    updated

---
+++ kernel-dev/bootstrap.sh
@@ -0,0 +1,10 @@
+# Update Packages
+apt-get update
+# Upgrade Packages
+apt-get upgrade -y
+
+sudo apt-get install git build-essential \
+  gawk wget git diffstat unzip \
+  texinfo gcc-multilib build-essential chrpath socat \
+  libsdl1.2-dev xterm ncurses-dev lzop libelf-dev make \
+  gcc binutils -y
